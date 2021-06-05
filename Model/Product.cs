981045303f697d37b243297e38ae52042194cc09
Jayakrishnan Ks <Jayakrishnan_KS@Dell.com>
2021-06-05 09:55:36 +0530

    Models

---
+++ Model/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Model
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+        public int SalePrice { get; set; }
+    }
+}
---
+++ Model/Promotion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Promotion
+    {
+        public IList<string> ProductNames { get; set; }
+        public bool IsCombo { get; set; }
+        public int Discount { get; set; }
+        public int Count { get; set; }
+    }
+
+}
