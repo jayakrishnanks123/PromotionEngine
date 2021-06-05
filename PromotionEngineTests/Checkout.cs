5afe5aeb4912e55d8574d83f893aebdf8b9fa00c
Jayakrishnan Ks <Jayakrishnan_KS@Dell.com>
2021-06-05 10:06:07 +0530

    Test1

---
+++ PromotionEngineTests/Checkout.cs
@@ -0,0 +1,23 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+
+namespace PromotionEngineTests
+{
+    internal class Checkout
+    {
+        public Checkout()
+        {
+        }
+
+        internal List<Product> CheckoutProducts(List<Product> orders)
+        {
+            return new List<Product>
+            {
+                new Product{ Name = "A", SalePrice = 50},
+                new Product{ Name = "B", SalePrice= 30},
+                new Product {Name = "C", SalePrice=20}
+            };
+        }
+    }
+}
---
+++ PromotionEngineTests/Tests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System.Collections.Generic;
+
+namespace PromotionEngineTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var Orders = new List<Product>
+            {
+                new Product{ Name = "A", Quantity = 1},
+                new Product{ Name = "B", Quantity = 1},
+                new Product {Name = "C", Quantity=1}
+            };
+            var response= new Checkout().CheckoutProducts(Orders);
+
+            Assert.AreEqual(response[0].SalePrice, 50);
+            Assert.AreEqual(response[1].SalePrice, 30);
+            Assert.AreEqual(response[2].SalePrice, 20);
+        }
+
+    }
+}
